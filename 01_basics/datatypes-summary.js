a56a634f60513eb83c57518813355b044f0a4e51
SARTHAKPANDEY1 <159184642+SARTHAKPANDEY1@users.noreply.github.com>
2026-01-01 13:21:25 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const marks =100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ const id  = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id === anotherId);
+
+ const bigNumber = 8888888888887777n
+
+
+
+//Reference (Non Premitive)
+
+// Arrays, Objects, Functions
+
+const heros = ["akshay","sidharth"]
+let myobj = {
+    name:"akks",
+    age: 33,
+}
+
+const myFunction = function name() {
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
