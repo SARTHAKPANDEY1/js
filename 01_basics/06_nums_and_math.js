2c39d5fb730a9e03a165642d70c8de3f0e8d8cfd
SARTHAKPANDEY1 <159184642+SARTHAKPANDEY1@users.noreply.github.com>
2026-01-03 05:54:57 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,45 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8965
+
+// console.log(otherNumber.toPrecision(4));
+
+const number= 1000000
+// console.log(number.toLocaleString('en-IN'));
+
+//Maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6)); // takes highest value
+// console.log(Math.floor(4.6));// takes lowest value 
+// console.log(Math.min(4.6,3,5));
+// console.log(Math.max(4.6,8,9));
+
+console.log(Math.random()); // between 0 and 1 values are printed
+console.log((Math.random()*10)+ 1);
+console.log(Math.floor(Math.random()*10)+ 1); // to avoid zero we used +1
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
