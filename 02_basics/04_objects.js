f2c1f7b2b4273a436a29461134374a2c540eb05a
SARTHAKPANDEY1 <159184642+SARTHAKPANDEY1@users.noreply.github.com>
2026-01-03 14:17:16 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -40,13 +40,36 @@ const users = [
     }
 ]
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "Js in english",
+    price: "999",
+    courseInstructor : "akshay"
+}
+// course.courseInstructor
+
+const {courseInstructor: ins} = course
+
+// console.log(courseInstructor);
+console.log(ins);
+
+// {
+//     "name": "aman",
+//     "coursename":"js",
+//     "price": "free"
+// }
 
 
+[
+    {},
+    {},
+    {}
+]
 
