81542220add0b153129cc2b4d9a0122b5e0fa84b
SARTHAKPANDEY1 <159184642+SARTHAKPANDEY1@users.noreply.github.com>
2026-01-04 14:45:18 +0000

    high order array loops

---
+++ 05_iterations/five.js
@@ -0,0 +1,38 @@
+const coding = ["js", "java","python","cpp"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=>{
+//     console.log(item , index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName : "js",
+        languageFile: "javas"
+    },
+    {
+        languageName : "java",
+        languageFile: "java"
+    },
+    {
+        languageName : "python",
+        languageFile: "py"
+    }
+]
+myCoding.forEach( (item)=> {
+
+    console.log(item.languageName);
+})
---
+++ 05_iterations/four.js
@@ -0,0 +1,24 @@
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: "ruby"
+// }
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const prog = ["js", "rb", "py", "java"]
+
+// for (const key in prog) {
+//     console.log(prog[key]);
+    
+// }
+
+// const map = new Map() (not suitable)
+// map.set('IN', "India")
+// map.set('USA', "United state of america")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
